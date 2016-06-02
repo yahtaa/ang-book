6d24daee54a21dcf80a8e7587011b4bf29d8802e
yahtaa <dmull01@gmail.com>
2016-06-01 22:56:17 -0400

    Added print page function on viewOrder

--- app/components/viewOrder/viewOrder.js
+++ app/components/viewOrder/viewOrder.js
@@ -7,12 +7,7 @@ angular.module('metalsSystemApp')
   $scope.order = Orders.get({orderId: $routeParams.orderId});
 
   $scope.printOrder = function(divName){
-    // var printContents = document.getElementById('order-to-print').innerHTML;
-    // var originalContents = document.body.innerHTML;
-    // document.body.innerHTML = printContents;
-    // console.log("SAVED ORDER")
-    // window.print();
-    // document.body.innerHTML = originalContents;
+    window.print();
   }
 
 

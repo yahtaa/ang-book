76f314118ddf090b27c619832f9113947fd73314
yahtaa <dmull01@gmail.com>
2016-06-09 15:15:54 -0400

    Added delete order to orderView

--- app/components/viewOrder/viewOrder.js
+++ app/components/viewOrder/viewOrder.js
@@ -2,14 +2,21 @@
 
 angular.module('metalsSystemApp')
 
-.controller('viewOrderCtrl', function($scope, Orders, $http, $routeParams){
+.controller('viewOrderCtrl', function($scope, Orders, $http, $location, $routeParams){
 
   $scope.order = Orders.get({orderId: $routeParams.orderId});
 
   $scope.printOrder = function(divName){
     window.print();
   }
-
+  $scope.deleteOrder = function(){
+    var confirmDelete = confirm("Are you sure you want to delete this order?");
+    if (confirmDelete === true) {
+      Orders.delete({orderId: $routeParams.orderId});
+      alert("Order Deleted!");
+      $location.path('#/');
+    }
+  }
 
 
 })
